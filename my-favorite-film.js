d69dd130f110eb9ecd1ad54c3f9c87945ee30994
Jules <praline40@hotmail.fr>
2020-06-12 04:54:09 -0400

    aa

---
+++ my-favorite-film.js
@@ -0,0 +1,10 @@
+/* my-favorite-film.js */
+
+// Define some variables
+const myFavoriteFilmName = 'Les visiteurs';
+const myFavoriteFilmDate = '1993';
+const myFavoriteFilmMaker = 'Jean-Marie Poiré';
+const message = 'My favorite film is "' + myFavoriteFilmName + '" a french film made by '+ myFavoriteFilmMaker + ' and was released in ' + myFavoriteFilmDate;
+
+// Display a popup
+alert(message);
