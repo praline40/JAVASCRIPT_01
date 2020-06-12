ba649ab736f40bb0460917762fa256e93b4b0320
Jules <praline40@hotmail.fr>
2020-06-12 04:57:09 -0400

    modif type number

--- my-favorite-film.js
+++ my-favorite-film.js
@@ -2,7 +2,7 @@
 
 // Define some variables
 const myFavoriteFilmName = 'Les visiteurs';
-const myFavoriteFilmDate = '1993';
+const myFavoriteFilmDate = 1993;
 const myFavoriteFilmMaker = 'Jean-Marie Poiré';
 const message = 'My favorite film is "' + myFavoriteFilmName + '" a french film made by '+ myFavoriteFilmMaker + ' and was released in ' + myFavoriteFilmDate;
 
